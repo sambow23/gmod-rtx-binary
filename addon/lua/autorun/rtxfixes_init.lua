99f40c4d841fe42e360cc6316bb332adc364851b
CR <sambow23@gmail.com>
2024-12-24 14:08:09 -0600

    fix light creation, remove default light on spawn

--- addon/lua/autorun/rtxfixes_init.lua
+++ addon/lua/autorun/rtxfixes_init.lua
@@ -1,6 +1,103 @@
 if (CLIENT) then
-require((BRANCH == "x86-64" or BRANCH == "chromium" ) and "RTXFixesBinary" or "RTXFixesBinary_32bit")
-hook.Add("PostDrawOpaqueRenderables", "rtx_fixes_render", function(depth, sky, sky3d)	--PreDrawViewModels
-	RTXDrawLights()
-end)
+    require((BRANCH == "x86-64" or BRANCH == "chromium" ) and "RTXFixesBinary" or "RTXFixesBinary_32bit")
+
+    -- Store light handles for each light entity
+    local lightHandles = {}
+
+    -- Debug function
+    local function DebugPrint(...)
+        if GetConVar("developer"):GetBool() then
+            print("[RTX Debug]", ...)
+        end
+    end
+
+    -- Hook into light entity creation/updates
+    hook.Add("OnEntityCreated", "rtx_fixes_light_creation", function(ent)
+        if not IsValid(ent) then return end
+        
+        -- Wait until entity is initialized
+        timer.Simple(0, function()
+            if not IsValid(ent) then return end
+            
+            if ent:GetClass() == "light" then
+                DebugPrint("Creating RTX light for entity:", ent:EntIndex())
+                -- Create RTX light for this entity
+                local pos = ent:GetPos()
+                local brightness = ent:GetBrightness() or 1
+                local size = ent:GetLightSize() or 100
+                local color = ent:GetColor()
+                
+                -- Convert color to 0-1 range
+                local r, g, b = color.r / 255, color.g / 255, color.b / 255
+                
+                DebugPrint("Light properties:", pos, brightness, size, color)
+                
+                -- Create RTX light handle
+                local handle = CreateRTXLight(pos.x, pos.y, pos.z, size, brightness * 100, r, g, b)  -- Increased brightness scaling
+                if handle then
+                    DebugPrint("Successfully created RTX light handle")
+                    lightHandles[ent:EntIndex()] = handle
+                else
+                    DebugPrint("Failed to create RTX light handle!")
+                end
+            end
+        end)
+    end)
+
+    -- Add console command to print debug info
+    concommand.Add("rtx_debug_lights", function()
+        print("=== RTX Light Debug Info ===")
+        print("Active RTX lights:", table.Count(lightHandles))
+        for entIdx, handle in pairs(lightHandles) do
+            local ent = Entity(entIdx)
+            if IsValid(ent) then
+                local pos = ent:GetPos()
+                print(string.format("Light %d: Pos(%d,%d,%d)", entIdx, pos.x, pos.y, pos.z))
+            end
+        end
+    end)
+
+    -- Update light properties when they change
+    local nextUpdate = 0
+    hook.Add("Think", "rtx_fixes_light_update", function()
+        -- Limit updates to every 0.1 seconds
+        if CurTime() < nextUpdate then return end
+        nextUpdate = CurTime() + 0.1
+
+        for entIdx, handle in pairs(lightHandles) do
+            local ent = Entity(entIdx)
+            if IsValid(ent) then
+                local pos = ent:GetPos()
+                local brightness = ent:GetBrightness() or 1
+                local size = ent:GetLightSize() or 100
+                local color = ent:GetColor()
+                
+                -- Update RTX light properties
+                local newHandle = UpdateRTXLight(handle, pos.x, pos.y, pos.z, size, brightness * 100, 
+                                               color.r / 255, color.g / 255, color.b / 255)
+                if newHandle then
+                    lightHandles[entIdx] = newHandle
+                end
+            else
+                DebugPrint("Removing RTX light for deleted entity:", entIdx)
+                DestroyRTXLight(handle)
+                lightHandles[entIdx] = nil
+            end
+        end
+    end)
+
+    -- Test command to spawn a bright RTX light at player position
+    concommand.Add("rtx_test_light", function(ply)
+        local pos = ply:GetPos()
+        local handle = CreateRTXLight(pos.x, pos.y, pos.z + 50, 200, 1000, 1, 1, 1)  -- Bright white light
+        if handle then
+            DebugPrint("Created test light at player position")
+        end
+    end)
+
+    hook.Add("PostDrawOpaqueRenderables", "rtx_fixes_render", function(depth, sky, sky3d)
+        DrawRTXLights()
+    end)
+
+    DebugPrint("RTX Fixes initialized")
 end
--- source/module.cpp
+++ source/module.cpp
@@ -1,5 +1,6 @@
 #include "GarrysMod/Lua/Interface.h"
 #include <remix.h>
+#include <vector>
 
 #include "cdll_client_int.h"	//IVEngineClient
 #include "materialsystem/imaterialsystem.h"
@@ -13,96 +14,212 @@ extern IShaderAPI* g_pShaderAPI = NULL;
 remix::Interface* g_remix = nullptr;
 remixapi_LightHandle g_scene_light = nullptr;
 
-/*
-	require "HelloWorld"
-	print( TestFunction( 5, 17 ) )
-*/
+// Add a vector to store all active light handles
+std::vector<remixapi_LightHandle> g_active_lights;
 
 using namespace GarrysMod::Lua;
 
-LUA_FUNCTION(RTXDrawLights)
+LUA_FUNCTION(CreateRTXLight)
+{
+    float x = LUA->CheckNumber(1);
+    float y = LUA->CheckNumber(2);
+    float z = LUA->CheckNumber(3);
+    float size = LUA->CheckNumber(4);
+    float brightness = LUA->CheckNumber(5);
+    float r = LUA->CheckNumber(6);
+    float g = LUA->CheckNumber(7);
+    float b = LUA->CheckNumber(8);
+
+    Msg("[RTX Remix Fixes] Creating light at (%f, %f, %f) with size %f and brightness %f\n", 
+        x, y, z, size, brightness);
+
+    auto sphereLight = remixapi_LightInfoSphereEXT{
+        REMIXAPI_STRUCT_TYPE_LIGHT_INFO_SPHERE_EXT,
+        nullptr,
+        {x, y, z},  // Position
+        size,       // Radius
+        false,
+        {},
+    };
+
+    auto lightInfo = remixapi_LightInfo{
+        REMIXAPI_STRUCT_TYPE_LIGHT_INFO,
+        &sphereLight,
+        0x3,
+        { r * brightness, g * brightness, b * brightness },
+    };
+     
+    auto lightHandle = g_remix->CreateLight(lightInfo);
+    if (!lightHandle) {
+        Msg("[RTX Remix Fixes] Failed to create light!\n");
+        LUA->ThrowError("[RTX Remix Fixes] - remix::CreateLight() failed");
+        return 0;
+    }
+
+    // Add the light to our active lights collection
+    g_active_lights.push_back(lightHandle.value());
+
+    Msg("[RTX Remix Fixes] Successfully created light handle\n");
+    LUA->PushUserdata(lightHandle.value());
+    return 1;
+}
+
+
+LUA_FUNCTION(UpdateRTXLight)
+{
+    auto handle = (remixapi_LightHandle)LUA->GetUserdata(1);
+    float x = LUA->CheckNumber(2);
+    float y = LUA->CheckNumber(3);
+    float z = LUA->CheckNumber(4);
+    float size = LUA->CheckNumber(5);
+    float brightness = LUA->CheckNumber(6);
+    float r = LUA->CheckNumber(7);
+    float g = LUA->CheckNumber(8);
+    float b = LUA->CheckNumber(9);
+
+    // Remove old handle from active lights
+    auto it = std::find(g_active_lights.begin(), g_active_lights.end(), handle);
+    if (it != g_active_lights.end()) {
+        g_active_lights.erase(it);
+    }
+
+    auto sphereLight = remixapi_LightInfoSphereEXT{
+        REMIXAPI_STRUCT_TYPE_LIGHT_INFO_SPHERE_EXT,
+        nullptr,
+        {x, y, z},
+        size,
+        false,
+        {},
+    };
+
+    auto lightInfo = remixapi_LightInfo{
+        REMIXAPI_STRUCT_TYPE_LIGHT_INFO,
+        &sphereLight,
+        0x3,
+        { r * brightness, g * brightness, b * brightness },
+    };
+
+    g_remix->DestroyLight(handle);
+    auto newHandle = g_remix->CreateLight(lightInfo);
+    if (!newHandle) {
+        LUA->ThrowError("[RTX Remix Fixes] - Failed to update light");
+        return 0;
+    }
+
+    // Add new handle to active lights
+    g_active_lights.push_back(newHandle.value());
+
+    LUA->PushUserdata(newHandle.value());
+    return 1;
+}
+
+LUA_FUNCTION(DestroyRTXLight)
+{
+    auto handle = (remixapi_LightHandle)LUA->GetUserdata(1);
+    
+    // Remove from active lights
+    auto it = std::find(g_active_lights.begin(), g_active_lights.end(), handle);
+    if (it != g_active_lights.end()) {
+        g_active_lights.erase(it);
+    }
+
+    g_remix->DestroyLight(handle);
+    return 0;
+}
+
+LUA_FUNCTION(DrawRTXLights)
 { 
-	g_remix->DrawLightInstance(g_scene_light); 
-	return 1;
+    static int frameCount = 0;
+    frameCount++;
+    
+    if (frameCount % 300 == 0) {  // Print every 300 frames
+        Msg("[RTX Remix Fixes] Drawing %d lights (frame %d)\n", g_active_lights.size(), frameCount);
+    }
+    
+    // Draw all active lights
+    for (const auto& light : g_active_lights) {
+        g_remix->DrawLightInstance(light);
+    }
+    
+    return 1;
 }
 
 GMOD_MODULE_OPEN()
 { 
-	Msg("[RTX Remix Fixes 2] - Module loaded!\n"); 
+    Msg("[RTX Remix Fixes 2] - Module loaded!\n"); 
 
-	LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB ); 
-		LUA->PushCFunction(RTXDrawLights);
-		LUA->SetField(-2, "RTXDrawLights"); // Set MyFirstFunction in lua to our C++ function
-	LUA->Pop();
+	g_active_lights.reserve(100);  // Reserve space for up to 100 lights
 
-	Msg("[RTX Remix Fixes 2] - Loading engine\n");
-	if (!Sys_LoadInterface("engine", VENGINE_CLIENT_INTERFACE_VERSION, NULL, (void**)&engine))
-		LUA->ThrowError("[RTX Remix Fixes 2] - Could not load engine interface");
+    LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB); 
+        LUA->PushCFunction(CreateRTXLight);
+        LUA->SetField(-2, "CreateRTXLight");
+        
+        LUA->PushCFunction(UpdateRTXLight);
+        LUA->SetField(-2, "UpdateRTXLight");
+        
+        LUA->PushCFunction(DestroyRTXLight);
+        LUA->SetField(-2, "DestroyRTXLight");
+        
+        LUA->PushCFunction(DrawRTXLights);
+        LUA->SetField(-2, "DrawRTXLights");
+    LUA->Pop();
 
-	Msg("[RTX Remix Fixes 2] - Loading materialsystem\n");
-	if (!Sys_LoadInterface("materialsystem", MATERIAL_SYSTEM_INTERFACE_VERSION, NULL, (void**)&materials))
-		LUA->ThrowError("[RTX Remix Fixes 2] - Could not load materialsystem interface"); 
+    Msg("[RTX Remix Fixes 2] - Loading engine\n");
+    if (!Sys_LoadInterface("engine", VENGINE_CLIENT_INTERFACE_VERSION, NULL, (void**)&engine))
+        LUA->ThrowError("[RTX Remix Fixes 2] - Could not load engine interface");
 
-	Msg("[RTX Remix Fixes 2] - Loading shaderapi\n"); 
-	g_pShaderAPI = (IShaderAPI*)materials->QueryInterface(SHADERAPI_INTERFACE_VERSION);
-	if (!g_pShaderAPI)
-		LUA->ThrowError("[RTX Remix Fixes 2] - Could not load shaderapi interface");
+    Msg("[RTX Remix Fixes 2] - Loading materialsystem\n");
+    if (!Sys_LoadInterface("materialsystem", MATERIAL_SYSTEM_INTERFACE_VERSION, NULL, (void**)&materials))
+        LUA->ThrowError("[RTX Remix Fixes 2] - Could not load materialsystem interface"); 
 
-	auto shaderapidx = GetModuleHandle("shaderapidx9.dll");
-	static const char sign[] = "BA E1 0D 74 5E 48 89 1D ?? ?? ?? ??";
-	auto ptr = ScanSign(shaderapidx, sign, sizeof(sign) - 1);
-	if (!ptr) { LUA->ThrowError("[RTX Remix Fixes 2] - Could find D3D9Device with sig"); }
+    Msg("[RTX Remix Fixes 2] - Loading shaderapi\n"); 
+    g_pShaderAPI = (IShaderAPI*)materials->QueryInterface(SHADERAPI_INTERFACE_VERSION);
+    if (!g_pShaderAPI)
+        LUA->ThrowError("[RTX Remix Fixes 2] - Could not load shaderapi interface");
 
-	auto offset = ((uint32_t*)ptr)[2];
-	auto m_pD3DDevice = *(IDirect3DDevice9Ex**)((char*)ptr + offset + 12);
-	if (!m_pD3DDevice) { LUA->ThrowError("[RTX Remix Fixes 2] - D3D9Device is null!!"); }
+    auto shaderapidx = GetModuleHandle("shaderapidx9.dll");
+    static const char sign[] = "BA E1 0D 74 5E 48 89 1D ?? ?? ?? ??";
+    auto ptr = ScanSign(shaderapidx, sign, sizeof(sign) - 1);
+    if (!ptr) { LUA->ThrowError("[RTX Remix Fixes 2] - Could find D3D9Device with sig"); }
 
-	Msg("[RTX Remix Fixes 2] - Loading remix dll\n");
-	 
-	if (auto interf = remix::lib::loadRemixDllAndInitialize(L"d3d9.dll")) {
-		g_remix = new remix::Interface{ *interf };
-	}
-	else {
-		LUA->ThrowError("[RTX Remix Fixes 2] - remix::loadRemixDllAndInitialize() failed"); 
-	}
+    auto offset = ((uint32_t*)ptr)[2];
+    auto m_pD3DDevice = *(IDirect3DDevice9Ex**)((char*)ptr + offset + 12);
+    if (!m_pD3DDevice) { LUA->ThrowError("[RTX Remix Fixes 2] - D3D9Device is null!!"); }
 
-	//MaterialAdapterInfo_t info;
-	//g_pShaderAPI->shader
+    Msg("[RTX Remix Fixes 2] - Loading remix dll\n");
+     
+    if (auto interf = remix::lib::loadRemixDllAndInitialize(L"d3d9.dll")) {
+        g_remix = new remix::Interface{ *interf };
+    }
+    else {
+        LUA->ThrowError("[RTX Remix Fixes 2] - remix::loadRemixDllAndInitialize() failed"); 
+    }
 
-	// supply IDirect3DDevice9Ex
-	// 
-	// HOW do i get this thing
-	g_remix->dxvk_RegisterD3D9Device(m_pD3DDevice); //g_pShaderAPI->GetD3DDevice() or Dx9Device() somehow?
+    g_remix->dxvk_RegisterD3D9Device(m_pD3DDevice);
 
-	//g_remix->SetConfigVariable("rtx.fallbackLightMode", "2");
+    // Configure RTX settings
+    if (g_remix) {
+        g_remix->SetConfigVariable("rtx.enableAdvancedMode", "1");
+        g_remix->SetConfigVariable("rtx.fallbackLightMode", "2");
+        Msg("[RTX Remix Fixes] RTX configuration set\n");
+    }
 
-	auto sphereLight = remixapi_LightInfoSphereEXT{
-		REMIXAPI_STRUCT_TYPE_LIGHT_INFO_SPHERE_EXT,
-		nullptr,
-		{0,0,0},
-		1024.0f,
-		false ,
-		{},
-	};
-	auto lightInfo = remixapi_LightInfo{
-		REMIXAPI_STRUCT_TYPE_LIGHT_INFO,
-		&sphereLight,
-		0x3,
-		{ 100, 200, 100 },
-	};
-	 
-	auto lightHandle = g_remix->CreateLight(lightInfo);
-	Msg("[RTX Remix Fixes 2] - creating light...\n");
-	if (!lightHandle) {
-		LUA->ThrowError("[RTX Remix Fixes 2] - remix::CreateLight() failed"); 
-	}
-
-	g_scene_light = lightHandle.value();
-
-	return 0;
+    return 0;
 }
 
 GMOD_MODULE_CLOSE()
 {
-	return 0;
+    // Clean up all lights
+    for (const auto& light : g_active_lights) {
+        if (g_remix && light) {
+            g_remix->DestroyLight(light);
+        }
+    }
+    g_active_lights.clear();
+
+    if (g_remix) {
+        delete g_remix;
+        g_remix = nullptr;
+    }
+    return 0;
 }
