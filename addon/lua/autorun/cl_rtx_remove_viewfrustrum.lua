7a4b16003b9e6260aafd902f5c405f7ae5de9888
CR <sambow23@gmail.com>
2025-01-04 22:36:04 -0600

    optimize viewfrustrum stuff

--- addon/lua/autorun/cl_rtx_remove_viewfrustrum.lua
+++ addon/lua/autorun/cl_rtx_remove_viewfrustrum.lua
@@ -1,5 +1,5 @@
--- Constants
-local HUGE_BOUNDS = 1e6 -- Large number for bounds
+local cv_disable_culling = CreateClientConVar("disable_frustum_culling", "1", true, false, "Disable frustum culling")
+local cv_bounds_size = CreateClientConVar("frustum_bounds_size", "10000", true, false, "Size of render bounds when culling is disabled (default: 1000)")
 
 -- Helper function to safely set render bounds
 local function SetHugeRenderBounds(ent)
@@ -13,8 +13,9 @@ local function SetHugeRenderBounds(ent)
     local model = ent:GetModel()
     if not model or model == "" then return end
     
-    local mins = Vector(-HUGE_BOUNDS, -HUGE_BOUNDS, -HUGE_BOUNDS)
-    local maxs = Vector(HUGE_BOUNDS, HUGE_BOUNDS, HUGE_BOUNDS)
+    local bounds_size = cv_bounds_size:GetFloat()
+    local mins = Vector(-bounds_size, -bounds_size, -bounds_size)
+    local maxs = Vector(bounds_size, bounds_size, bounds_size)
     
     ent:SetRenderBounds(mins, maxs)
 end
@@ -30,17 +31,43 @@ hook.Add("OnEntityCreated", "SetHugeRenderBounds", function(ent)
     end)
 end)
 
--- Add ConVar for enabling/disabling
-local cv_disable_culling = CreateClientConVar("disable_frustum_culling", "1", true, false, "Disable frustum culling")
-
 -- Add command to toggle
 concommand.Add("toggle_frustum_culling", function()
     cv_disable_culling:SetBool(not cv_disable_culling:GetBool())
     print("Frustum culling: " .. (cv_disable_culling:GetBool() and "DISABLED" or "ENABLED"))
 end)
 
+-- Add command to adjust bounds size
+concommand.Add("set_frustum_bounds", function(ply, cmd, args)
+    if not args[1] then 
+        print("Current bounds size: " .. cv_bounds_size:GetFloat())
+        return
+    end
+    
+    local new_size = tonumber(args[1])
+    if not new_size then
+        print("Invalid size value. Please use a number.")
+        return
+    end
+    
+    cv_bounds_size:SetFloat(new_size)
+    print("Set frustum bounds size to: " .. new_size)
+    
+    -- Force refresh of all entity bounds
+    for _, ent in ipairs(ents.GetAll()) do
+        SetHugeRenderBounds(ent)
+    end
+end)
+
 -- Monitor convar changes
 cvars.AddChangeCallback("disable_frustum_culling", function(name, old, new)
+    -- Force refresh of all entity bounds
+    for _, ent in ipairs(ents.GetAll()) do
+        SetHugeRenderBounds(ent)
+    end
+end)
+
+cvars.AddChangeCallback("frustum_bounds_size", function(name, old, new)
     -- Force refresh of all entity bounds
     for _, ent in ipairs(ents.GetAll()) do
         SetHugeRenderBounds(ent)
@@ -76,9 +103,10 @@ hook.Add("InitPostEntity", "SetupRenderBoundsOverride", function()
     end
 end)
 
--- Debug command to print entity info
+-- Debug command to print entity info (removed duplicate declaration)
 concommand.Add("debug_render_bounds", function()
     print("\nEntity Render Bounds Debug:")
+    print("Current bounds size: " .. cv_bounds_size:GetFloat())
     for _, ent in ipairs(ents.GetAll()) do
         if IsValid(ent) and ent.SetRenderBounds then
             local model = ent:GetModel() or "no model"
